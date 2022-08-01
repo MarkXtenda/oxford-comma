4cdefae9714c54cc3f1b67ac4f5c965e7b3b0e89
MarkXtenda <mark.i2p.1998@gmail.com>
2022-08-01 20:39:09 +1000

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
+    oxford_comma = ""
+    oxford_last = ""
 
+    if (array.size == 1)
+        oxford_comma = array.join
+    elsif (array.size == 2)
+        oxford_last = array[-1]
+        array.pop
+        oxford_comma = array.join()
+        oxford_comma << " and " << oxford_last
+    else 
+        oxford_last = array[-1]
+        array.pop
+        oxford_comma = array.join(", ")
+        oxford_comma << ", and " << oxford_last
+    end
+    
+    oxford_comma
 end
+
+puts oxford_comma([
+    'antifungal',
+    'antifungals',
+    'bifunctional',
+    'cofunction',
+    'cofunctions'])
+
